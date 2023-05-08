d6a0c7f154efef3a65898155e838dc4ea109bb41
iqwa1107 <ramahs49@gmail.com>
2023-05-08 11:47:41 +0700

    int main

--- friendKelas/friendKelas.cpp
+++ friendKelas/friendKelas.cpp
@@ -32,3 +32,11 @@ void orang::setNama(string pNama) {
 void siswa::setId(int pId) {
 	id = pId;
 }
+int main() {
+	orang joko;
+	joko.setNama("Joko Kumat");
+	siswa joko_siswa;
+	joko_siswa.setId(1);
+	joko_siswa.displayAll(joko);
+	return 0;
+}
